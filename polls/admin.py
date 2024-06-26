92b68b7ae2f284c50f145c546db34cb4dfa76dc0
ccasas20 <test@test.com>
2024-06-26 09:57:49 +0200

    final updates activitat1

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Question,Choice
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question text", {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+class ChoiceTest(admin.ModelAdmin):
+    fieldsets = [
+        ("Question Selection", {"fields": ["question"]}),
+        ("Choice Information", {"fields": ["choice_text"],}),
+        (None, {"fields": ["votes"]}),
+    ]
+
+    
+    
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceTest)
